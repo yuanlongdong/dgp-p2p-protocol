5bce4d46818910668b03ef723c940353c115a291
yuanlongdong <97449554+yuanlongdong@users.noreply.github.com>
2026-03-05 11:57:48 +0800

    test(kleros): extend mock arbitrator to invoke adapter ruling relay

--- packages/contracts/contracts/MockArbitrator.sol
+++ packages/contracts/contracts/MockArbitrator.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+interface IKlerosAdapterRelay {
+function relayRuling(uint256 externalDisputeId, uint16 sellerBps) external;
+}
+
 contract MockArbitrator {
 uint256 public nextDisputeId;
 event DisputeCreated(uint256 indexed disputeId, uint256 choices, bytes extraData);
@@ -8,5 +12,9 @@ event DisputeCreated(uint256 indexed disputeId, uint256 choices, bytes extraData
 function createDispute(uint256 choices, bytes calldata extraData) external payable returns (uint256 disputeId) {
 disputeId = ++nextDisputeId;
 emit DisputeCreated(disputeId, choices, extraData);
+}
+
+function giveRuling(address adapter, uint256 externalDisputeId, uint16 sellerBps) external {
+IKlerosAdapterRelay(adapter).relayRuling(externalDisputeId, sellerBps);
 }
 }
