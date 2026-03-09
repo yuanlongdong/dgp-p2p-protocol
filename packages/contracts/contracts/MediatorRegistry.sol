6e5a3f74587bcc1b7091d323015202466c38080d
yuanlongdong <97449554+yuanlongdong@users.noreply.github.com>
2026-03-09 13:56:49 +0800

    fix(contracts): align mediator registry pause semantics

--- packages/contracts/contracts/MediatorRegistry.sol
+++ packages/contracts/contracts/MediatorRegistry.sol
@@ -2,15 +2,29 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract MediatorRegistry is Ownable {
-mapping(address => bool) public isMediator;
+contract MediatorRegistry is Ownable, Pausable {
+mapping(address => bool) private mediatorAllowed;
 event MediatorSet(address indexed mediator, bool allowed);
 
 constructor(address initialOwner) Ownable(initialOwner) {}
 
 function setMediator(address mediator, bool allowed) external onlyOwner {
-isMediator[mediator] = allowed;
+mediatorAllowed[mediator] = allowed;
 emit MediatorSet(mediator, allowed);
+}
+
+function isMediator(address mediator) external view returns (bool) {
+if (paused()) return false;
+return mediatorAllowed[mediator];
+}
+
+function pause() external onlyOwner {
+_pause();
+}
+
+function unpause() external onlyOwner {
+_unpause();
 }
 }
